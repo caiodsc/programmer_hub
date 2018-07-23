c0015522b278f753a19f57c7e84edaa71cf27deb
Caio <caio.dscamara@gmail.com>
2018-07-22 20:58:22 -0400

    Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  has_many :likes
+
+  acts_as_followable
+  acts_as_follower
 end
