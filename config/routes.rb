f93a7090bb3105b4b2c70fe4a4023feec6e5dffd
Caio <caio.dscamara@gmail.com>
2018-07-22 22:04:29 -0400

    Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'comments/update'
   #devise_for :users
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
